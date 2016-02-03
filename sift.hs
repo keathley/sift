ba4dd5996a0a40089e08fdc813d1a3cb44b73c5d
Chris Keathley <c@keathley.io>
2016-02-02 23:11:58 -0500

    Start working on writing to tty

--- sift.hs
+++ sift.hs
@@ -1,4 +1,6 @@
 import Data.List
+import System.IO
+import System.Environment
 
 type Query  = String
 type Choice = String
@@ -27,9 +29,10 @@ scoreChoices :: Query -> [Choice] -> [(Choice, Score)]
 scoreChoices q = map (lowestScore q)
 
 lowestScore :: Query -> Choice -> (Choice, Score)
-lowestScore query choice = (choice, score)
-  where
-    score = minimum $ wordScores query choice
+lowestScore query choice =
+  case wordScores query choice of
+    []     -> (choice, infinity)
+    scores -> (choice, minimum scores)
 
 wordScores :: String -> String -> [Score]
 wordScores [] _ = []
@@ -45,6 +48,7 @@ wordScore (Found (q:qs) (i:is) score index)
   | q == i && index == 1  = wordScore (Sequential qs is (score+1) index)
   | q == i                = wordScore (Found qs is (score+index) 1)
   | i == '/'              = wordScore (Boundary (q:qs) is score (index+1))
+  | i == '.'              = wordScore (Boundary (q:qs) is score (index+1))
   | otherwise             = wordScore (Found (q:qs) is score (index+1))
 wordScore (Found [] _ score _)      = score
 wordScore (Boundary [] _ score _)   = score
@@ -61,17 +65,45 @@ wordScore(Sequential (q:qs) (i:is) score index)
   | q == i    = wordScore (Sequential qs is score 1)
   | otherwise = wordScore (Found (q:qs) is score (index+1))
 
-main :: IO()
-main = do
-  print $ lowestScore "ama" "app/models/america" -- ->3
-  print $ lowestScore "ame" "app/models/america"-- ->2
-  print $ lowestScore "amer" "app/models/america"-- ->2
-  print $ lowestScore "ami" "app/models/america"-- ->5
-  print $ lowestScore "amn" "app/models/nepal"-- ->3
-  print $ lowestScore "amp" "app/models/nepal"-- ->11
-  print $ bestChoice "ama" ["app/models/america", "app/models/nepal"]
-  print $ bestChoice "amn" ["app/models/america", "app/models/nepal"]
-  print $ bestChoice "app/m/ep" ["app/models/america", "app/models/nepal"]
-  print $ bestChoice "ama" ["app/models/america", "app/models/nepal", "app/models/animals"]
-  print $ bestChoice "ami" ["app/models/america", "app/models/nepal", "app/models/animals"]
-  print $ bestChoice "ani" ["app/models/america", "app/models/nepal", "app/models/animals"]
+splitInput :: String -> [Choice]
+splitInput input =
+  let allLines = lines input
+      result = allLines
+  in result
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String ()
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>> ") evalAndPrint
+
+main :: IO ()
+main = do args <- getArgs
+          case length args of
+            0 -> runRepl
+            1 -> evalAndPrint $ head args
+            otherwise -> putStrLn "Program takes only 0 or 1 argument"
+
+-- main :: IO()
+-- main = do
+--   contents <- getContents
+--   tty <- openFile "/dev/tty" ReadMode
+--   putStr contents
+--   selection <- hGetLine tty
+--   putStrLn $ bestChoice selection (splitInput contents)
